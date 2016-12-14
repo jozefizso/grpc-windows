4cc952bc5f45485df31906e57de92c8f7dc7312e
Jozef Izso <jozef.izso@gmail.com>
2016-12-14 13:41:04 +0100

    Sign output release executables

--- sign-executables.bat
+++ sign-executables.bat
@@ -1,2 +1,10 @@
-signtool.exe sign /v /sha1 AB19D2855D203E5AEEE13584AD3AAD1CA9FBAC3D /sm /t http://timestamp.verisign.com/scripts/timstamp.dll protoc.exe
-signtool.exe sign /as /v /fd sha256 /td sha256 /sha1 AB19D2855D203E5AEEE13584AD3AAD1CA9FBAC3D /sm /tr http://timestamp.comodoca.com/rfc3161 protoc.exe
+
+set _signkey=AB19D2855D203E5AEEE13584AD3AAD1CA9FBAC3D
+
+signtool.exe sign /v /sha1 %_signkey% /sm /t http://timestamp.verisign.com/scripts/timstamp.dll grpc\bin\protobuf\release\protoc.exe
+signtool.exe sign /as /v /fd sha256 /td sha256 /sha1 %_signkey% /sm /tr http://timestamp.comodoca.com/rfc3161 grpc\bin\protobuf\release\protoc.exe
+
+
+signtool.exe sign /v /sha1 %_signkey% /sm /t http://timestamp.verisign.com/scripts/timstamp.dll grpc\bin\grpc_protoc_plugins\grpc_cpp_plugin.exe
+signtool.exe sign /as /v /fd sha256 /td sha256 /sha1 %_signkey% /sm /tr http://timestamp.comodoca.com/rfc3161 grpc\bin\grpc_protoc_plugins\grpc_cpp_plugin.exe
+
