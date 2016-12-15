53abd1aa2427a9d97b979c9626de537df1b663db
Jozef Izso <jozef.izso@gmail.com>
2016-12-15 21:50:15 +0100

    Fix sign-executables to work outside of VS command prompt

--- sign-executables.bat
+++ sign-executables.bat
@@ -1,6 +1,8 @@
 
 set _signkey=AB19D2855D203E5AEEE13584AD3AAD1CA9FBAC3D
 
+call "%VS140COMNTOOLS%..\..\VC\vcvarsall.bat" amd64_x86
+
 signtool.exe sign /v /sha1 %_signkey% /sm /t http://timestamp.verisign.com/scripts/timstamp.dll grpc\bin\protobuf\release\protoc.exe
 signtool.exe sign /as /v /fd sha256 /td sha256 /sha1 %_signkey% /sm /tr http://timestamp.comodoca.com/rfc3161 grpc\bin\protobuf\release\protoc.exe
 
